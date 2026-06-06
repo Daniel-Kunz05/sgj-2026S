94f3d78ebdccbd0dd9aeb5bab88ef1015888eddb
Thuy-Vy Nghiem <thuyvynghiem@yahoo.com>
2026-06-06 15:57:46 +0200

    particle setup

---
+++ semester-game-jam-2026s/12 Particle System/ParticleFreer.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class ParticleFreer : GpuParticles2D
+{
+	ulong timeCreated;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		timeCreated = Time.GetTicksMsec();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Time.GetTicksMsec() - timeCreated > 10000)
+			QueueFree();
+	}
+}
